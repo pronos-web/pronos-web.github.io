abde474841d8da2291a69f42e8df175a5d007672
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-02-01 23:21:55 -0800

    More testing

--- Scripts/index.js
+++ Scripts/index.js
@@ -1077,13 +1077,13 @@ document.getElementById('theList').addEventListener("click", function(e) {
  *  
  *****************************************************************/
 // Collapsible div for filters
-var coll = document.getElementsByClassName("collapsible");
-var i;
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
