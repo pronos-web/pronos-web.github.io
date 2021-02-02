7f455c99f65352b21d2f4fefcf21aa9decb73f8d
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-02-01 23:14:21 -0800

    Added js for collapsible filter div

--- Scripts/index.js
+++ Scripts/index.js
@@ -1076,3 +1076,18 @@ document.getElementById('theList').addEventListener("click", function(e) {
  *  Testing New Ideas
  *  
  *****************************************************************/
+// Collapsible div for filters
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
