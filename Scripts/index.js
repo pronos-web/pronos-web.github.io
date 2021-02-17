94d7681679435dcae2116983bd9d5859f8993627
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-02-16 23:30:09 -0800

    Testing bacground color of tool link button

--- Scripts/index.js
+++ Scripts/index.js
@@ -190,7 +190,11 @@ function showAll() {
                 <td class="rmvBtn">
                     <button type="button" id="removeItem" value="${tool.id}">X</button>
                 </td>
-            </tr>`;
+            </tr>
+            <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            `;
             }
         }  
     );
