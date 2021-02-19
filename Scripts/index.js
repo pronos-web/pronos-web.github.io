d310308ae02ddfe6647c788041502ac5184a97e4
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-02-19 05:40:31 -0800

    testing collapsible div

--- Scripts/index.js
+++ Scripts/index.js
@@ -1111,7 +1111,8 @@ let i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    let content = this.nextElementSibling;
+    //let content = this.nextElementSibling; collapsible
+    let content = collapsible.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
