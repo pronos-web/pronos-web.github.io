0a725f1a25150cfc3e974914e24bda8e96852ca2
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-02-23 23:22:19 -0800

    Added missing }

--- Scripts/index.js
+++ Scripts/index.js
@@ -79,12 +79,12 @@ function addNewItem() {
         document.querySelector('#tool1').innerHTML = "Tool Type *";
         document.querySelector('#tool1').style.color = 'red';
         document.querySelector('#numb').style.width = '35%';
+    }
     if(document.getElementById('toolTyp').value === ""){
         //Add prompt that field is required
         document.querySelector('#toolBld').innerHTML = "Building *";
         document.querySelector('#bld1').style.color = 'red';
         document.querySelector('#bay').style.width = '35%';
-
     } 
     else{
         itemList.push(tool);
