3e30742687505cb9672dc2bd620bf061d2d7e87c
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-01 23:51:10 -0800

    Added firebase libraries

--- Scripts/fbscript.js
+++ Scripts/fbscript.js
@@ -1,6 +1,7 @@
-document.querySelector('#addBtn').addEventListener('click', saveData);
 
 function saveData() {
     var firebaseRef = firebase.database().ref();
     firebaseRef.child("Text").set("testing data upload");
 }
+
+document.querySelector('#addBtn').addEventListener('click', saveData);
