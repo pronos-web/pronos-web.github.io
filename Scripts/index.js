e98731f51089923d3df5401737e4a74b66c07137
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-14 23:10:29 -0700

    testing

--- Scripts/index.js
+++ Scripts/index.js
@@ -21,7 +21,7 @@ function  saveToFirebase(a, b, c, d, e, f, g) {
     console.log("saveToFirebase() started")
     //Creates the ID LSA403 and assigns values to it
     var dbContent = firebase.database();
-    var test01 = dbContent.ref().child('Tool').child().get();
+    var test01 = dbContent.ref().child('Tool').child();
     console.log("This is the format of the data: ");
     console.log(test01);
     var toolContent = dbContent.ref().child('Tool').child(g).set({
