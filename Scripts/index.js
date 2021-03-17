0329f9ff3d2b2ddf6e6c37a0c0189d85c5d12f1f
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-17 15:40:03 -0700

    edit 'X'

--- Scripts/index.js
+++ Scripts/index.js
@@ -54,38 +54,33 @@ var databaseRef = firebase.database().ref("Tool");
 databaseRef.on('child_added', function(snapshot) {
     var item = snapshot.val(); 
 
-    // add the event to the UI
-    // var elm = document.createElement('li');
-    // elm.id = 'item-'+ snapshot.key;
-    // elm.innerText = snapshot.key + " " + item.Building + " " + item.Bay;
-    // document.querySelector('#item-list').appendChild(elm);
-
-    // add the event to the Table
+    // add row to the Table
     var elm1 = document.createElement('tr')
     document.querySelector('#newBody').appendChild(elm1);
+    
+    // add cell to the table row
     var elm2 = document.createElement('td');
-   // elm2.id = 'elm2-'+ snapshot.key;
     elm2.setAttribute("id", elm2.id);
-    //elm2.innerText = snapshot.key;
+
+    // define button attributes
     var btn = document.createElement('button', 'snapshot.key');
     btn.setAttribute("id", 'elm2-'+ snapshot.key);
     console.log(btn.id);
     btn.setAttribute("class", "tButton");
     btn.setAttribute("type", "button");
-
     btn.onclick = function() { toolButtons(snapshot.key) };
 
+    // add buttion to cell
     document.querySelector('#newBody').appendChild(btn)
-    //document.querySelector('#newBody').appendChild(elm2);
-
     document.querySelector('#'+btn.id).textContent = snapshot.key;
     
+    // add second cell with building and bay
     var elm3 = document.createElement('td');
     elm3.id = 'elm3-'+ snapshot.key;
     elm3.innerText = item.Building + " " + item.Bay;
     document.querySelector('#newBody').appendChild(elm3);
 
-    // add the event to our list
+    // add the tool and details to the items list
     items.push({
         Tool: snapshot.key,
         Building: item.Building, 
@@ -93,49 +88,46 @@ databaseRef.on('child_added', function(snapshot) {
     });
 });
 
-var items = [];
-var databaseRef = firebase.database().ref("Tool");
-databaseRef.on('child_removed', function(snapshot) {
+function editList() {
+    var databaseRef = firebase.database().ref("Tool");
+databaseRef.on('child_added', function(snapshot) {
     var item = snapshot.val(); 
 
-    // add the event to the UI
-    // var elm = document.createElement('li');
-    // elm.id = 'item-'+ snapshot.key;
-    // elm.innerText = snapshot.key + " " + item.Building + " " + item.Bay;
-    // document.querySelector('#item-list').appendChild(elm);
-
-    // add the event to the Table
+    // add row to the Table
     var elm1 = document.createElement('tr')
     document.querySelector('#newBody').appendChild(elm1);
+    
+    // add cell to the table row
     var elm2 = document.createElement('td');
-   // elm2.id = 'elm2-'+ snapshot.key;
     elm2.setAttribute("id", elm2.id);
-    //elm2.innerText = snapshot.key;
+
+    // define button attributes
     var btn = document.createElement('button', 'snapshot.key');
     btn.setAttribute("id", 'elm2-'+ snapshot.key);
     console.log(btn.id);
     btn.setAttribute("class", "tButton");
     btn.setAttribute("type", "button");
-
     btn.onclick = function() { toolButtons(snapshot.key) };
 
+    // add buttion to cell
     document.querySelector('#newBody').appendChild(btn)
-    //document.querySelector('#newBody').appendChild(elm2);
-
     document.querySelector('#'+btn.id).textContent = snapshot.key;
     
+    // add second cell with building and bay
     var elm3 = document.createElement('td');
     elm3.id = 'elm3-'+ snapshot.key;
     elm3.innerText = item.Building + " " + item.Bay;
     document.querySelector('#newBody').appendChild(elm3);
 
-    // add the event to our list
-    items.push({
-        Tool: snapshot.key,
-        Building: item.Building, 
-        Bay: item.Bay
-    });
+    // add third cell to delete the item from the list
+    var elm4 = document.createElement('td');
+    elm4.id = 'elm4-'+ snapshot.key;
+    elm4.innerText = "X";
+    elm4.setAttribute("color", "#ff0000");
+    document.querySelector('#newBody').appendChild(elm4);
+
 });
+}
 
 function updateList() {
 
@@ -1286,6 +1278,7 @@ function showEdit() {
     filter = 3;
     // Reset filters so all tools are visible after edit
     clearFilters();
+    editList();
 
     // Hide fileter buttons
     document.getElementById('filterTable').style.display = "none";
