0d4586e5786fd7b9e92bd1f8a2cb2b86cdd082c4
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-17 13:44:06 -0700

    Attempt to ad text to button

--- Scripts/index.js
+++ Scripts/index.js
@@ -65,13 +65,14 @@ databaseRef.on('child_added', function(snapshot) {
     var elm1 = document.createElement('tr')
     document.querySelector('#newBody').appendChild(elm1);
     var elm2 = document.createElement('td');
-    elm2.id = 'elm2-'+ snapshot.key;
-    elm2.setAttribute("id", "leftTD");
+    //elm2.id = 'elm2-'+ snapshot.key;
+    elm2.setAttribute("id", elm2.id);
     //elm2.innerText = snapshot.key;
     var btn = document.createElement('button', "snot");
     btn.setAttribute("id", "btoolBtn");
     btn.setAttribute("class", "tButton");
     btn.setAttribute("type", "button");
+    document.querySelector('#addBtn').textContent = snapshot.key;
 
     btn.onclick = function() { toolButtons(snapshot.key) };
 
