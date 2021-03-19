5ea91cc9a485c42215cc524be04c202b63269b6f
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-18 20:24:58 -0700

    try

--- Scripts/index.js
+++ Scripts/index.js
@@ -96,7 +96,7 @@ function creatTableHeader() {
     // format cells
     document.getElementById("cell1").style.fontWeight = "900";
     document.getElementById("cell2").style.fontWeight = "900";
-    document.getElementById("row1").style.borderBottom  = "thin solid #000000";
+    document.getElementById("row1").style.borderBottom  = "solid #000000";
 }
 
 function formatTable() {
