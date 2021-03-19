c2ded9e161abc331bd90c27671fe62ace5be1a4d
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-18 20:42:55 -0700

    adjust width when editing

--- Scripts/index.js
+++ Scripts/index.js
@@ -65,9 +65,6 @@ databaseRef.on('child_added', function(snapshot) {
     elm3.innerText = item.Building + " " + item.Bay;
     document.querySelector('#newBody').appendChild(elm3);
 
-    document.getElementById(elm2.id).style.borderBottom  = "solid #000000";
-    document.getElementById(elm3.id).style.borderBottom  = "solid #000000";
-
     // add the tool and details to the items list
     items.push({
         Tool: snapshot.key,
@@ -161,6 +158,8 @@ function editList() {
     elm4.innerText = "X";
     elm4.setAttribute("style", "color: #ff0000;");
     document.querySelector('#newBody').appendChild(elm4);
+
+    document.getElementById('#'+btn.id).style.paddingRight = "130px";
 
     // add the tool and details to the items list
     items.push({
