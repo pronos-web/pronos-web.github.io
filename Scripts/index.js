ceedcd4e3823ebcaab3f17a561fa4e31bbfb4e41
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-19 20:44:39 -0700

    fixed line 17083 from == to ++

--- Scripts/index.js
+++ Scripts/index.js
@@ -17080,7 +17080,7 @@ document.getElementById('listBody').addEventListener("click", function(e) {
 let coll = document.getElementsByClassName("collapsible");
 let i;
 
-for (i = 0; i < coll.length; i==) {
+for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let content = this.nextElementSibling;
