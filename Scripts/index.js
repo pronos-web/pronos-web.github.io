05d77c09424d69c838f230efa48eabcb6dfc8bbe
jlharris70 <48006299+jlharris70@users.noreply.github.com>
2021-03-31 21:42:59 -0700

    Comment out toolExist(toolID) line 21859

--- Scripts/index.js
+++ Scripts/index.js
@@ -21856,7 +21856,7 @@ function addNewItem() {
         && document.getElementById('numb').value != ""){
             toolID = toolID + toolNumb;
             //See if tool aready exist
-            toolExist(toolID);
+            //toolExist(toolID);
             if(exist == true){
                 console.log("Aborting Add"); 
                 return;
